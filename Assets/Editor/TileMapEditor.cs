2949bd2fc1f15e41d1c31ce1eed397fdfb9970dc
gouki04 <gouki04@126.com>
2015-04-28 21:01:56 +0800

    优化编辑器，将tile的选择放入TileMapWindow

--- Assets/Editor/TileMapEditor.cs
+++ Assets/Editor/TileMapEditor.cs
@@ -6,106 +6,13 @@ using System.Collections.Generic;
 
 namespace MagicTower.Editor
 {
-    public abstract class SpriteList<T> : List<T>
-    {
-        public int Margin = 1;
-        public int Border = 1;
-        public int TileWidth = 32;
-        public int TileHeight = 32;
-        public int TilePerRow = 12;
-
-        public int SelectedIndex = -1;
-
-        protected abstract Sprite GetSprite(int index);
-
-        public bool Draw()
-        {
-            var sheet = SpriteSheetManager.Instance;
-
-            int row = (int)Mathf.Ceil(Count / (float)TilePerRow);
-
-            var rect = GUILayoutUtility.GetRect(Border * 2 + TileWidth * TilePerRow + Margin * (TilePerRow - 1),
-                Border * 2 + TileHeight * row + Margin * (row - 1));
-
-            rect.width = TileWidth;
-            rect.height = TileHeight;
-
-            var origin_x = rect.x;
-            var origin_y = rect.y;
-
-            bool selected_tile = false;
-            for (int r = 0; r < row; ++r)
-            {
-                rect.x = origin_x + Border;
-                rect.y = origin_y + Border + r * TileHeight;
-
-                if (r > 0)
-                    rect.y += (r - 1) * Margin;
-
-                for (int c = 0; c < TilePerRow; ++c)
-                {
-                    var index = TilePerRow * r + c;
-                    if (index >= Count)
-                        break;
-
-                    var sprite = GetSprite(index);
-
-                    var texRect = new Rect(
-                        sprite.textureRect.xMin / sprite.texture.width,
-                        sprite.textureRect.yMin / sprite.texture.height,
-                        sprite.textureRect.width / sprite.texture.width,
-                        sprite.textureRect.height / sprite.texture.height);
-
-                    if (SelectedIndex == index)
-                        GUI.color = Color.yellow;
-                    else
-                        GUI.color = Color.white;
-
-                    GUI.DrawTextureWithTexCoords(rect, sprite.texture, texRect);
-
-                    Event e = Event.current;
-                    if (e.type == EventType.MouseDown)
-                    {
-                        if (rect.Contains(e.mousePosition))
-                            SelectedIndex = index;
-
-                        selected_tile = true;
-                    }
-
-                    rect.x += TileWidth + Margin;
-                }
-            }
-
-            return selected_tile;
-        }
-    }
-
-    public class MonsterList : SpriteList<uint>
-    {
-        protected override Sprite GetSprite(int index)
-        {
-            var monster_id = this[index];
-            var sprite_name = CSVManager.Instance["monster"][monster_id]["sprite"];
-            return SpriteSheetManager.Instance[sprite_name];
-        }
-    }
-
-    public class ItemList : SpriteList<uint>
-    {
-        protected override Sprite GetSprite(int index)
-        {
-            var item_id = this[index];
-            var sprite_name = CSVManager.Instance["item"][item_id]["sprite"];
-            return SpriteSheetManager.Instance[sprite_name];
-        }
-    }
+    
 
     [CustomEditor(typeof(EditorData.TileMap))]
     public class TileMapEditor : UnityEditor.Editor
     {
         EditorData.TileMap tilemap;
-        MonsterList mMonsterList;
-        ItemList mItemList;
+        
 
         public void OnEnable()
         {
@@ -113,19 +20,8 @@ namespace MagicTower.Editor
 
             SceneView.onSceneGUIDelegate += TileMapUpdate;
 
-            SpriteSheetManager.Instance.Load("tile");
 
-            mMonsterList = new MonsterList();
-            for (uint i = 1; i <= 30; ++i)
-            {
-                mMonsterList.Add(i);
-            }
-
-            mItemList = new ItemList();
-            for (uint i = 1; i <= 7; ++i)
-            {
-                mItemList.Add(i);
-            }
+            
         }
 
         void TileMapUpdate(SceneView scene_view)
@@ -149,7 +45,11 @@ namespace MagicTower.Editor
                 var obj = new GameObject("Tile");
 
                 var renderer = obj.AddComponent<SpriteRenderer>();
-                renderer.sprite = SpriteSheetManager.Instance.GetSprite("wall_0");
+
+                if (Selecting.SelectedList is MonsterList)
+                    renderer.sprite = (Selecting.SelectedList as MonsterList).GetSprite(Selecting.SelectedIndex);
+                else if (Selecting.SelectedList is ItemList)
+                    renderer.sprite = (Selecting.SelectedList as ItemList).GetSprite(Selecting.SelectedIndex);
 
                 obj.transform.parent = tilemap.transform;
                 obj.transform.localPosition = local_pos;
@@ -158,15 +58,7 @@ namespace MagicTower.Editor
 
         public override void OnInspectorGUI()
         {
-            var sheet = SpriteSheetManager.Instance;
-
-            EditorGUILayout.LabelField("Monster List", EditorStyles.boldLabel);
-            if (mMonsterList.Draw())
-                Repaint();
-
-            EditorGUILayout.LabelField("Item List", EditorStyles.boldLabel);
-            if (mItemList.Draw())
-                Repaint();
+            
 
             SceneView.RepaintAll();
         }
--- Assets/Editor/TileMapWindow.cs
+++ Assets/Editor/TileMapWindow.cs
@@ -1,28 +1,147 @@
 using UnityEngine;
 using UnityEditor;
 using System.Collections;
+using System.Collections.Generic;
+using Utils;
 
-public class TileMapWindow : EditorWindow
+namespace MagicTower.Editor
 {
-    string myString = "Hello World";
-    bool groupEnabled;
-    bool myBool = true;
-    float myFloat = 1.23f;
-
-    [MenuItem("Window/TileMapWindow")]
-    public static void ShowWindow()
+    public class Selecting
     {
-        EditorWindow.GetWindow(typeof(TileMapWindow));
+        public static object SelectedList;
+        public static int SelectedIndex;
     }
 
-    void OnGUI()
+    public abstract class SpriteList<T> : List<T>
     {
-        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
-        myString = EditorGUILayout.TextField("Text Field", myString);
+        public int Margin = 1;
+        public int Border = 1;
+        public int TileWidth = 32;
+        public int TileHeight = 32;
+        public int TilePerRow = 12;
 
-        groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
-        myBool = EditorGUILayout.Toggle("Toggle", myBool);
-        myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
-        EditorGUILayout.EndToggleGroup();
+        public abstract Sprite GetSprite(int index);
+
+        public bool Draw()
+        {
+            int row = (int)Mathf.Ceil(Count / (float)TilePerRow);
+
+            var rect = GUILayoutUtility.GetRect(Border * 2 + TileWidth * TilePerRow + Margin * (TilePerRow - 1),
+                Border * 2 + TileHeight * row + Margin * (row - 1));
+
+            rect.width = TileWidth;
+            rect.height = TileHeight;
+
+            var origin_x = rect.x;
+            var origin_y = rect.y;
+
+            bool selected_tile = false;
+            for (int r = 0; r < row; ++r)
+            {
+                rect.x = origin_x + Border;
+                rect.y = origin_y + Border + r * TileHeight;
+
+                if (r > 0)
+                    rect.y += (r - 1) * Margin;
+
+                for (int c = 0; c < TilePerRow; ++c)
+                {
+                    var index = TilePerRow * r + c;
+                    if (index >= Count)
+                        break;
+
+                    var sprite = GetSprite(index);
+
+                    var texRect = new Rect(
+                        sprite.textureRect.xMin / sprite.texture.width,
+                        sprite.textureRect.yMin / sprite.texture.height,
+                        sprite.textureRect.width / sprite.texture.width,
+                        sprite.textureRect.height / sprite.texture.height);
+
+                    if (Selecting.SelectedList == this && Selecting.SelectedIndex == index)
+                        GUI.color = Color.yellow;
+                    else
+                        GUI.color = Color.white;
+
+                    GUI.DrawTextureWithTexCoords(rect, sprite.texture, texRect);
+
+                    Event e = Event.current;
+                    if (e.type == EventType.MouseDown)
+                    {
+                        if (rect.Contains(e.mousePosition))
+                        {
+                            Selecting.SelectedList = this;
+                            Selecting.SelectedIndex = index;
+                        }
+
+                        selected_tile = true;
+                    }
+
+                    rect.x += TileWidth + Margin;
+                }
+            }
+
+            return selected_tile;
+        }
     }
+
+    public class MonsterList : SpriteList<uint>
+    {
+        public override Sprite GetSprite(int index)
+        {
+            var monster_id = this[index];
+            var sprite_name = CSVManager.Instance["monster"][monster_id]["sprite"];
+            return SpriteSheetManager.Instance[sprite_name];
+        }
+    }
+
+    public class ItemList : SpriteList<uint>
+    {
+        public override Sprite GetSprite(int index)
+        {
+            var item_id = this[index];
+            var sprite_name = CSVManager.Instance["item"][item_id]["sprite"];
+            return SpriteSheetManager.Instance[sprite_name];
+        }
+    }
+
+    public class TileMapWindow : EditorWindow
+    {
+        MonsterList mMonsterList;
+        ItemList mItemList;
+
+        [MenuItem("Window/TileMapWindow")]
+        public static void ShowWindow()
+        {
+            EditorWindow.GetWindow(typeof(TileMapWindow));
+        }
+
+        void OnEnable()
+        {
+            SpriteSheetManager.Instance.Load("tile");
+
+            mMonsterList = new MonsterList();
+            for (uint i = 1; i <= 30; ++i)
+            {
+                mMonsterList.Add(i);
+            }
+
+            mItemList = new ItemList();
+            for (uint i = 1; i <= 7; ++i)
+            {
+                mItemList.Add(i);
+            }
+        }
+
+        void OnGUI()
+        {
+            EditorGUILayout.LabelField("Monster List", EditorStyles.boldLabel);
+            if (mMonsterList.Draw())
+                Repaint();
+
+            EditorGUILayout.LabelField("Item List", EditorStyles.boldLabel);
+            if (mItemList.Draw())
+                Repaint();
+        }
+    } 
 }
