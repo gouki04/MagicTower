e13e899f46741e512fe72069943f0282e39d1151
gouki04 <gouki04@126.com>
2015-05-06 23:20:23 +0800

    修复sprite editor的bug 整理生成prefab的代码

--- Assets/Editor/MonsterAnimationGeneratorWindow.cs
+++ Assets/Editor/MonsterAnimationGeneratorWindow.cs
@@ -5,6 +5,101 @@ using UnityEngine;
 
 namespace MagicTower.Editor
 {
+    public class Helper
+    {
+        public static AnimationClip CreateAnimationClip(
+            string animation_name, 
+            float animation_duration, 
+            float frame_rate, 
+            string[] frame_names, 
+            bool loop, 
+            string asset_path)
+        {
+            AnimationClip clip = new AnimationClip();
+            AnimationUtility.SetAnimationType(clip, ModelImporterAnimationType.Generic);
+
+            EditorCurveBinding curve_binding = new EditorCurveBinding();
+            curve_binding.type = typeof(SpriteRenderer);
+            curve_binding.path = "";
+            curve_binding.propertyName = "m_Sprite";
+
+            ObjectReferenceKeyframe[] key_frames = new ObjectReferenceKeyframe[3];
+
+            float frame_time = animation_duration / frame_names.Length;
+            for (int i = 0; i < frame_names.Length; ++i)
+            {
+                var sprite = Utils.SpriteSheetManager.Instance.GetSprite(frame_names[i]);
+
+                key_frames[i] = new ObjectReferenceKeyframe();
+                key_frames[i].time = frame_time * i;
+                key_frames[i].value = sprite;
+            }
+
+            clip.frameRate = frame_rate;
+
+            if (loop)
+            {
+                //设置为循环动画
+                SerializedObject serialized_clip = new SerializedObject(clip);
+                AnimationClipSettings clip_settings = new AnimationClipSettings(serialized_clip.FindProperty("m_AnimationClipSettings"));
+                clip_settings.loopTime = true;
+                serialized_clip.ApplyModifiedProperties();
+            }
+
+            AnimationUtility.SetObjectReferenceCurve(clip, curve_binding, key_frames);
+            AssetDatabase.CreateAsset(clip, asset_path);
+            AssetDatabase.SaveAssets();
+
+            return clip;
+        }
+
+        public static AnimatorController CreateEmptyAnimatorController(string asset_path)
+        {
+            return AnimatorController.CreateAnimatorControllerAtPath(asset_path);
+        }
+
+        public static AnimatorController CreateSimpleAnimatorController(AnimationClip clip, string asset_path)
+        {
+            var animator_controller = CreateEmptyAnimatorController(asset_path);
+
+            var layer = animator_controller.GetLayer(0);
+            var sm = layer.stateMachine;
+
+            var state = sm.AddState(clip.name);
+            state.SetAnimationClip(clip, layer);
+
+            AssetDatabase.SaveAssets();
+
+            return animator_controller;
+        }
+
+        class AnimationClipSettings
+        {
+            SerializedProperty m_Property;
+
+            private SerializedProperty Get(string property) { return m_Property.FindPropertyRelative(property); }
+
+            public AnimationClipSettings(SerializedProperty prop) { m_Property = prop; }
+
+            public float startTime { get { return Get("m_StartTime").floatValue; } set { Get("m_StartTime").floatValue = value; } }
+            public float stopTime { get { return Get("m_StopTime").floatValue; } set { Get("m_StopTime").floatValue = value; } }
+            public float orientationOffsetY { get { return Get("m_OrientationOffsetY").floatValue; } set { Get("m_OrientationOffsetY").floatValue = value; } }
+            public float level { get { return Get("m_Level").floatValue; } set { Get("m_Level").floatValue = value; } }
+            public float cycleOffset { get { return Get("m_CycleOffset").floatValue; } set { Get("m_CycleOffset").floatValue = value; } }
+
+            public bool loopTime { get { return Get("m_LoopTime").boolValue; } set { Get("m_LoopTime").boolValue = value; } }
+            public bool loopBlend { get { return Get("m_LoopBlend").boolValue; } set { Get("m_LoopBlend").boolValue = value; } }
+            public bool loopBlendOrientation { get { return Get("m_LoopBlendOrientation").boolValue; } set { Get("m_LoopBlendOrientation").boolValue = value; } }
+            public bool loopBlendPositionY { get { return Get("m_LoopBlendPositionY").boolValue; } set { Get("m_LoopBlendPositionY").boolValue = value; } }
+            public bool loopBlendPositionXZ { get { return Get("m_LoopBlendPositionXZ").boolValue; } set { Get("m_LoopBlendPositionXZ").boolValue = value; } }
+            public bool keepOriginalOrientation { get { return Get("m_KeepOriginalOrientation").boolValue; } set { Get("m_KeepOriginalOrientation").boolValue = value; } }
+            public bool keepOriginalPositionY { get { return Get("m_KeepOriginalPositionY").boolValue; } set { Get("m_KeepOriginalPositionY").boolValue = value; } }
+            public bool keepOriginalPositionXZ { get { return Get("m_KeepOriginalPositionXZ").boolValue; } set { Get("m_KeepOriginalPositionXZ").boolValue = value; } }
+            public bool heightFromFeet { get { return Get("m_HeightFromFeet").boolValue; } set { Get("m_HeightFromFeet").boolValue = value; } }
+            public bool mirror { get { return Get("m_Mirror").boolValue; } set { Get("m_Mirror").boolValue = value; } }
+        }
+    }
+
     /// <summary>
     /// 基于文章《Unity2D研究院之自动生成动画、AnimationController、Prefab（一）》
     /// http://www.xuanyusong.com/archives/3243
@@ -26,45 +121,47 @@ namespace MagicTower.Editor
 
         AnimationClip GenerateAnimationClip(int index)
         {
-            string animation_name = "monster_" + index;
-            AnimationClip clip = new AnimationClip();
-            AnimationUtility.SetAnimationType(clip, ModelImporterAnimationType.Generic);
+            return Helper.CreateAnimationClip("monster_" + index,
+                1f, 30f, new string[] { "monster_" + index, "monster_" + index + "_1", "monster_" + index }, true, string.Format("{0}monster_{1}.anim", ANIMATION_PATH, index));
 
-            EditorCurveBinding curve_binding = new EditorCurveBinding();
-            curve_binding.type = typeof(SpriteRenderer);
-            curve_binding.path = "";
-            curve_binding.propertyName = "m_Sprite";
+            //string animation_name = "monster_" + index;
+            //AnimationClip clip = new AnimationClip();
+            //AnimationUtility.SetAnimationType(clip, ModelImporterAnimationType.Generic);
 
-            ObjectReferenceKeyframe[] key_frames = new ObjectReferenceKeyframe[3];
+            //EditorCurveBinding curve_binding = new EditorCurveBinding();
+            //curve_binding.type = typeof(SpriteRenderer);
+            //curve_binding.path = "";
+            //curve_binding.propertyName = "m_Sprite";
 
-            float frame_time = 1 / 3f;
-            for (int i = 0; i < 3; ++i)
-            {
-                Sprite sprite = null;
-                if (i == 0 || i == 2)
-                    sprite = Utils.SpriteSheetManager.Instance.GetSprite("monster_" + index);
-                else if (i == 1)
-                    sprite = Utils.SpriteSheetManager.Instance.GetSprite("monster_" + index + "_1");
+            //ObjectReferenceKeyframe[] key_frames = new ObjectReferenceKeyframe[3];
 
-                key_frames[i] = new ObjectReferenceKeyframe();
-                key_frames[i].time = frame_time * i;
-                key_frames[i].value = sprite;
-            }
+            //float frame_time = 1 / 3f;
+            //for (int i = 0; i < 3; ++i)
+            //{
+            //    Sprite sprite = null;
+            //    if (i == 0 || i == 2)
+            //        sprite = Utils.SpriteSheetManager.Instance.GetSprite("monster_" + index);
+            //    else if (i == 1)
+            //        sprite = Utils.SpriteSheetManager.Instance.GetSprite("monster_" + index + "_1");
 
-            clip.frameRate = 30;
-            //clip.wrapMode = WrapMode.Loop;
+            //    key_frames[i] = new ObjectReferenceKeyframe();
+            //    key_frames[i].time = frame_time * i;
+            //    key_frames[i].value = sprite;
+            //}
 
-            //设置为循环动画
-            SerializedObject serialized_clip = new SerializedObject(clip);
-            AnimationClipSettings clip_settings = new AnimationClipSettings(serialized_clip.FindProperty("m_AnimationClipSettings"));
-            clip_settings.loopTime = true;
-            serialized_clip.ApplyModifiedProperties();
+            //clip.frameRate = 30;
 
-            AnimationUtility.SetObjectReferenceCurve(clip, curve_binding, key_frames);
-            AssetDatabase.CreateAsset(clip, string.Format("{0}monster_{1}.anim", ANIMATION_PATH, index));
-            AssetDatabase.SaveAssets();
+            ////设置为循环动画
+            //SerializedObject serialized_clip = new SerializedObject(clip);
+            //AnimationClipSettings clip_settings = new AnimationClipSettings(serialized_clip.FindProperty("m_AnimationClipSettings"));
+            //clip_settings.loopTime = true;
+            //serialized_clip.ApplyModifiedProperties();
 
-            return clip;
+            //AnimationUtility.SetObjectReferenceCurve(clip, curve_binding, key_frames);
+            //AssetDatabase.CreateAsset(clip, string.Format("{0}monster_{1}.anim", ANIMATION_PATH, index));
+            //AssetDatabase.SaveAssets();
+
+            //return clip;
         }
 
         AnimatorController GenerateAnimationController(List<AnimationClip> clips, int index)
@@ -105,10 +202,14 @@ namespace MagicTower.Editor
 
             for (int i = BeginIndex; i <= EndIndex; ++i)
             {
-                var clips = new List<AnimationClip>();
-                clips.Add(GenerateAnimationClip(i));
+                //var clips = new List<AnimationClip>();
+                //clips.Add(GenerateAnimationClip(i));
                 
-                var animator_controller = GenerateAnimationController(clips, i);
+                //var animator_controller = GenerateAnimationController(clips, i);
+                var clip = GenerateAnimationClip(i);
+                var animator_controller = Helper.CreateSimpleAnimatorController(clip, 
+                    string.Format("{0}monster_{1}.controller", ANIMATOR_CONTROLLER_PATH, i));
+
                 GeneratePrefab(animator_controller, i);
             }
         }
@@ -132,30 +233,6 @@ namespace MagicTower.Editor
             }
         }
 
-        class AnimationClipSettings
-        {
-            SerializedProperty m_Property;
-
-            private SerializedProperty Get(string property) { return m_Property.FindPropertyRelative(property); }
-
-            public AnimationClipSettings(SerializedProperty prop) { m_Property = prop; }
-
-            public float startTime { get { return Get("m_StartTime").floatValue; } set { Get("m_StartTime").floatValue = value; } }
-            public float stopTime { get { return Get("m_StopTime").floatValue; } set { Get("m_StopTime").floatValue = value; } }
-            public float orientationOffsetY { get { return Get("m_OrientationOffsetY").floatValue; } set { Get("m_OrientationOffsetY").floatValue = value; } }
-            public float level { get { return Get("m_Level").floatValue; } set { Get("m_Level").floatValue = value; } }
-            public float cycleOffset { get { return Get("m_CycleOffset").floatValue; } set { Get("m_CycleOffset").floatValue = value; } }
-
-            public bool loopTime { get { return Get("m_LoopTime").boolValue; } set { Get("m_LoopTime").boolValue = value; } }
-            public bool loopBlend { get { return Get("m_LoopBlend").boolValue; } set { Get("m_LoopBlend").boolValue = value; } }
-            public bool loopBlendOrientation { get { return Get("m_LoopBlendOrientation").boolValue; } set { Get("m_LoopBlendOrientation").boolValue = value; } }
-            public bool loopBlendPositionY { get { return Get("m_LoopBlendPositionY").boolValue; } set { Get("m_LoopBlendPositionY").boolValue = value; } }
-            public bool loopBlendPositionXZ { get { return Get("m_LoopBlendPositionXZ").boolValue; } set { Get("m_LoopBlendPositionXZ").boolValue = value; } }
-            public bool keepOriginalOrientation { get { return Get("m_KeepOriginalOrientation").boolValue; } set { Get("m_KeepOriginalOrientation").boolValue = value; } }
-            public bool keepOriginalPositionY { get { return Get("m_KeepOriginalPositionY").boolValue; } set { Get("m_KeepOriginalPositionY").boolValue = value; } }
-            public bool keepOriginalPositionXZ { get { return Get("m_KeepOriginalPositionXZ").boolValue; } set { Get("m_KeepOriginalPositionXZ").boolValue = value; } }
-            public bool heightFromFeet { get { return Get("m_HeightFromFeet").boolValue; } set { Get("m_HeightFromFeet").boolValue = value; } }
-            public bool mirror { get { return Get("m_Mirror").boolValue; } set { Get("m_Mirror").boolValue = value; } }
-        }
+        
     }
 }
--- Assets/Editor/SpriteEditor.cs
+++ Assets/Editor/SpriteEditor.cs
@@ -3,56 +3,64 @@ using UnityEditor;
 using System.IO;
 using System.Collections.Generic;
 
-public class SpriteEditor : EditorWindow
+namespace MagicTower.Editor
 {
-    private Texture2D mTexture;
-    private string mPlistPath;
-
-    [MenuItem("Window/SpriteEditor")]
-    public static void ShowWindow()
+    public class SpriteEditor : EditorWindow
     {
-        EditorWindow.GetWindow(typeof(SpriteEditor));
-    }
-	
-    void OnEnable()
-    { }
+        private Texture2D mTexture;
+        private string mPlistPath;
 
-    void OnGUI()
-    {
-        mTexture = (Texture2D)EditorGUILayout.ObjectField("Sprite", mTexture, typeof(Texture2D), false);
-
-        EditorGUILayout.BeginVertical();
-        mPlistPath = EditorGUILayout.TextField(mPlistPath);
-        if (GUILayout.Button("Init From Plist"))
+        [MenuItem("Window/SpriteEditor")]
+        public static void ShowWindow()
         {
-            var path = AssetDatabase.GetAssetPath(mTexture);
-            var texture_importer = AssetImporter.GetAtPath(path) as TextureImporter;
-            texture_importer.isReadable = true;
-
-            var meta_data = new List<SpriteMetaData>();
-
-            var json_content = File.ReadAllText(mPlistPath);
-            var json = SimpleJSON.JSON.Parse(json_content);
-
-            var width = json["meta"]["size"]["w"].AsInt;
-            var height = json["meta"]["size"]["h"].AsInt;
-
-            var frames = json["frames"].AsArray;
-            foreach (SimpleJSON.JSONNode frame in frames)
-            {
-                var sprite_meta_data = new SpriteMetaData();
-                sprite_meta_data.alignment = 9;
-                sprite_meta_data.border = new Vector4(0, 0, 0, 0);
-                sprite_meta_data.pivot = new Vector2(0, 0);
-                sprite_meta_data.rect = new Rect(frame["frame"]["x"].AsFloat, frame["frame"]["y"].AsFloat, frame["frame"]["w"].AsFloat, frame["frame"]["h"].AsFloat);
-                sprite_meta_data.name = frame["filename"].Value.Replace(".png", "");
-
-                meta_data.Add(sprite_meta_data);
-            }
-
-            texture_importer.spritesheet = meta_data.ToArray();
-            AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
+            EditorWindow.GetWindow(typeof(SpriteEditor));
         }
-        EditorGUILayout.EndVertical();
-    }
+
+        void OnEnable()
+        { }
+
+        void OnGUI()
+        {
+            mTexture = (Texture2D)EditorGUILayout.ObjectField("Sprite", mTexture, typeof(Texture2D), false);
+
+            EditorGUILayout.BeginVertical();
+            mPlistPath = EditorGUILayout.TextField(mPlistPath);
+            if (GUILayout.Button("Init From Plist"))
+            {
+                var path = AssetDatabase.GetAssetPath(mTexture);
+                var texture_importer = AssetImporter.GetAtPath(path) as TextureImporter;
+                texture_importer.isReadable = true;
+
+                var meta_data = new List<SpriteMetaData>();
+
+                var json_content = File.ReadAllText(mPlistPath);
+                var json = SimpleJSON.JSON.Parse(json_content);
+
+                var width = json["meta"]["size"]["w"].AsInt;
+                var height = json["meta"]["size"]["h"].AsInt;
+
+                var frames = json["frames"].AsArray;
+                foreach (SimpleJSON.JSONNode frame in frames)
+                {
+                    var sprite_meta_data = new SpriteMetaData();
+                    sprite_meta_data.alignment = 9;
+                    sprite_meta_data.border = new Vector4(0, 0, 0, 0);
+                    sprite_meta_data.pivot = new Vector2(0, 0);
+
+                    sprite_meta_data.rect = new Rect(
+                        frame["frame"]["x"].AsFloat, 
+                        (height - frame["frame"]["y"].AsFloat - frame["frame"]["h"].AsFloat), 
+                        frame["frame"]["w"].AsFloat, frame["frame"]["h"].AsFloat);
+
+                    sprite_meta_data.name = frame["filename"].Value.Replace(".png", "");
+
+                    meta_data.Add(sprite_meta_data);
+                }
+
+                texture_importer.spritesheet = meta_data.ToArray();
+                AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
+            }
+            EditorGUILayout.EndVertical();
+        }
+    } 
 }
